46f402f886a44e6019887a48ee12cee95c38ca44
bathobile <pearlbathobile.com>
2023-06-15 18:27:51 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A printf  function is used to prints a line on this program
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
