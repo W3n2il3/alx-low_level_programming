0e93e94aa1226fe8146d5b72852b7ea42997ce48
bathobile <pearlbathobile.com>
2023-06-16 18:28:54 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,10 +16,9 @@ int main(void)
 	w = n % 10;
 	if (w > 5)
 		printf("Last digits of %d is %d and is greater than 5\n", n, w);
-	if (w == 0)
+	else if (w == 0)
 		printf("Last digits of %d is %d and is 0\n", n, w);
-	if (w < 6 && w != 0)
+	else (w < 6 && w != 0)
 		printf("Last digitd of %d id %d and is less than 6 and not0\n", n, w);
-
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char b;
+
+	char p;
+
+	b = 'a';
+	p = 'A';
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	while
+		(p <= 'Z') {
+			putchar(p);
+			p++;
+		}
+	putchar('\n');
+	return (0);
+}
