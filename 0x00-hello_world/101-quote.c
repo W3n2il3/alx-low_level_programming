972ad3c25d313fa359cae1a9c52b2912f2c10912
bathobile <pearlbathobile.com>
2023-06-17 03:43:32 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main-unique standard error
+ * Return: 0
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful4\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
