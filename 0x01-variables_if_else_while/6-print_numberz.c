43cdffa609abc6c0a9a808410c11b7cae9013bf3
bathobile <pearlbathobile.com>
2023-06-18 08:15:26 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all the single number
+ * Return: 0
+ */
+
+int main(void)
+{
+	int base_ten = 0;
+
+	while (base_ten <= 10)
+	{
+		putchar (base_ten);
+		0++
+	}
+	putchar('\n');
+	return (0);
+}
