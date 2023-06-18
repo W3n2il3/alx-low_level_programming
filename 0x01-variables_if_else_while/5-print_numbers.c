65ab1f8d3be57ff8966bedbac1b0f5bac57dd932
bathobile <pearlbathobile.com>
2023-06-17 22:22:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the program that print numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+int base_ten = 0;
+
+while (base_ten <= 10)
+	{
+		putchar(base_ten);
+		base_ten++;
+	}
+	putchar('\n');
+return (0);
+}
