497d94c5d00e4301cf1623dd1d21f234c46d2539
bathobile <pearlbathobile.com>
2023-06-18 18:02:25 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 
 /**
- * main - the entry point 
+ * main - the entry point
  * Return: 0
  */
 
 int main(void)
 {
-        char letters;
+char letters;
 
-        for (letters = 'z' ; letters >= 'a' ; letters--)
-        {
-                putchar(letters);
-        }
-        putchar('\n');
-        return (0);
+for (letters = 'z' ; letters >= 'a' ; letters--)
+putchar(letters);
+putchar('\n');
+return (0);
 }
