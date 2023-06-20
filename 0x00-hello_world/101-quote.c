033080ddb4bf88317a0429be57915b68ba7c24b3
bathobile <pearlbathobile.com>
2023-06-19 19:30:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
+
 /**
- * main-unique standard error
- * Return: 0
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 
 int main(void)
+
 {
-	fputs("and that piece of art is useful4\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+
 }
