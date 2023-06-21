3efe6a9b6854dffee95546f1704d9dd91da1082b
bathobile <pearlbathobile.com>
2023-06-21 10:07:07 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *
- * _islpha - check for the alphabet charector
+ * _isalpha - check for the alphabet charector
  *  @c: the charector to be checked
  *  Return: 1 for alphabert charector or 0 for anything else
  */
 
 int _isalpha(int c)
+
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
