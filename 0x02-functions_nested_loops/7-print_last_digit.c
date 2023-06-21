7a14767f30234f00e7b0111bbc959dad792c9172
bathobile <pearlbathobile.com>
2023-06-21 16:44:55 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_the_last_digit - print the last digit of a number
- * @n:The number to be treated
- * Return: value of the last digits
- */
+* print_the_last_digit - print the last digit of a number
+* @e:The number to be treated
+* Return: value of the last digits
+*/
 
-int print_last_digit(int n)
+int print_last_digit(int e)
 {
-	int last;
+int last;
 
-	last = n % 10;
-	if (last < 0)
-	{
-		last = last * -1;
-	}
-	_putchar(last + '0');
-	return (last);
+last = e % 10;
+if (last < 0)
+{
+last = last * -1;
+}
+_putchar(last + '0');
+return (last);
 }
