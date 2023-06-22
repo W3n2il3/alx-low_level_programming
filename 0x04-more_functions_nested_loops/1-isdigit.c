5ae9099b16b2f9e29b145e205dfd9549559224e8
bathobile <pearlbathobile.com>
2023-06-22 11:06:52 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,13 @@
 
 /**
  * _isdigit - check if the charecter is digit
- * @x - the number to be checked
+ * @x: the number to be checked
  * Return:1 a digit charecter or 0 anything else
  */
 
 int _isdigit(int x)
 {
 	if (x >= 48 && x <= 57)
-	{
 		return (1);
-	}
 	return (0);
 }
