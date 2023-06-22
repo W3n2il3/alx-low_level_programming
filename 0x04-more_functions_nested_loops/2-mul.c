cb863a0f1fce6d2c47b9734b030675eb7e300331
bathobile <pearlbathobile.com>
2023-06-22 11:30:00 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul -multiples wo integers
+ *@x: the first number to be multiply
+ *@y: the second number to be multiply by the first number
+ *Return: the result of multiplication of x and y paran
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+
+}
