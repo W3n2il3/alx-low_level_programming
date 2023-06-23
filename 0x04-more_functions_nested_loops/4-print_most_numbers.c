179664362e874b2a8487303cbfc6717d95e6a757
bathobile <pearlbathobile.com>
2023-06-23 12:33:23 -0700

    0-main.c, main.h

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,21 +3,23 @@
 /**
  * print_most_numbers - print the number from 0 to 9
  * discription: primt the number that includes 2 and 4
+ * _putchar - to print charecter
  * Return: the number
  */
 
 void print_most_numbers(void)
 {
 	int p = 0;
+
 	for (; p <= 9; p++)
 	{
 		if ((p == 2) || (p == 4))
 		{
 			continue;
 		}
-		else 
+		else
 		{
-			_putchar(p + '0');
+			_putchar(p);
 		}
 	}
 	_putchar('\n');
