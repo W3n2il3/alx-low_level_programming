4c47fdfef8126b60c8ca90fe6238b9d97546a4be
bathobile <pearlbathobile.com>
2023-06-24 13:41:16 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more numbers- print 10 times from 0 up to 14
+ * Return: 10 times of the numbers
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+	{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+						}
+						_putchar((y % 10) + '0');
+		}
+		_putchar('\n')
+	}
+}
