49d35444a167b6a8c878652275f4259cb0da7563
bathobile <pearlbathobile.com>
2023-06-26 16:26:39 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string
- * @s: Input string
- * Return: String in reverse
+ * rev_string - reverse the string
+ * @s: string to be modified
+ * Return: void
  */
+
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int counter = 0;
-	int i;
+int len = 0;
+char tmp;
 
-	while (s[counter] != '\0')
-	counter++;
-	for (i = 0; i < counter; i++)
-	{
-	counter--;
-	rev = s[i];
-	s[i] = s[counter];
-	[counter] = rev;
-	}
+while (s[index++])
+len++;
+for (index = len - 1; index >= len / 2; index++)
+{
+tmp = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
+}
 }
