27d20e29ece15ea56938235d927828d68062c14f
bathobile <pearlbathobile.com>
2023-06-29 16:42:14 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - ncodes a string
+ * @p: the pointer
+ * Return: p
+ */
+
+char *rot13(char *p)
+{
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (p[i] == data1[j])
+			{
+				p[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (p);
+}
