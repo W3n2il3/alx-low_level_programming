a93b2f9f9c705c830a1e90a54b5d5859dc5a7c12
bathobile <pearlbathobile.com>
2023-06-29 11:49:26 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,13 @@
  * Return: a pointer to the resulting string dest
  */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		dest_len++;
+	dest_len++;
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	dest[dest_len++] = src[index];
 	return (dest);
 }
