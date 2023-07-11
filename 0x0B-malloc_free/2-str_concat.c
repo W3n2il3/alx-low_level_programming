837e6c7f22c9fc96e4cc1eb235cc75a65453e71c
bathobile <pearlbathobile.com>
2023-07-11 12:45:53 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat -   concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: the two string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
+
+
+	if (s1 == NULL)
+		s1 =  "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s2[i])
+		i++;
+
+	while (s2[j])
+		j++;
+	i = i + j;
+	s =  malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
+		return (NULL);
+	while (k < l)
+	{
+		if (k <= i)
+			s[k] = s1[k];
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
+}
