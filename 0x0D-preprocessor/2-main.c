f5df6e038c3e9c40627cdc598f2348ab8f69b453
bathobile <pearlbathobile.com>
2023-07-17 11:52:24 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print the name of the file
+ * main - prints the name of the file
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("%s\n", __file__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
